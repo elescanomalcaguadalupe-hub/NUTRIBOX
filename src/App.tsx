296e454ef8086a5c5d1c1de34e99c5654595db09
Guadalupe Elescano Malca <elescanomalcaguadalupe@gmail.com>
2026-05-28 14:34:10 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,8 @@ export default function App() {
 
   // User student credentials state initialized with user info
   const [user, setUser] = useState<UserProfile>({
-    name: "Usuario7626",
-    email: "usuario7626@gmail.com",
+    name: "Usuario",
+    email: "usuario@gmail.com",
     phone: "+51 987 654 321",
     address: "",
     photoUrl: "",
